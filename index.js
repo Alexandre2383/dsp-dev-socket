91c6c90488def2e19942ce3d25bab10e3d22b58e
Alexandre2383 <a.detroy@hotmail.fr>
2024-06-18 19:28:03 +0200

    Ajout de la modal pour rentrer son pseudo lié par un cookie

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const middle = require('./utils/utils')
 const { createServer } = require('node:http')
 const { join } = require('node:path')
 const { Server } = require('socket.io')
+const cookieParser = require('cookie-parser') // add cookie parser
 
 const app = express()
 const port = 5000
@@ -10,6 +11,8 @@ const server = createServer(app)
 const io = new Server(server)
 
 app.use(express.static(join(__dirname, 'public')))
+
+app.use(cookieParser())
 
 app.get('/', middle, function (req, res) {
   return res.sendFile(join(__dirname, 'public', 'index.html'))
@@ -20,17 +23,36 @@ app.get('/chat', (req, res) => {
 })
 
 app.get('/chat/:channel', (req, res) => {
+  // if the cookie is set
+  const pseudo = req.cookies.pseudo
+  if (!pseudo) {
+    return res.redirect('/')
+  }
   res.sendFile(join(__dirname, 'public', 'chat.html'))
 })
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+  console.log(socket.id)
 
-  socket.on('joinChannel', (channel) => {
+  // get the pseudo set by the user
+  socket.on('setPseudo', (pseudo) => {
+    socket.pseudo = pseudo // bind the pseudo with socket
+    console.log(`User ${socket.id} set pseudo to: ${pseudo}`)
+  })
+
+  // connexion to channel
+  socket.on('joinChannel', ({ channel, pseudo }) => {
+    socket.pseudo = pseudo
     socket.join(channel)
-    console.log(`User joined channel: ${channel}`)
+    console.log(`User ${pseudo} joined channel: ${channel}`)
+
     socket.on('message', (message) => {
-      io.to(channel).emit('receptMessage', { msg: message, id: socket.id })
+      io.to(channel).emit('receptMessage', {
+        msg: message,
+        id: socket.id,
+        pseudo: socket.pseudo,
+      })
     })
   })
 
